b92436e76e32a502934842bf32bfd9a40bce54bc
mumei-iff <mumei-iff@users.noreply.github.com>
2025-03-23 20:35:20 +0800

    update

--- init.lua
+++ init.lua
@@ -673,7 +673,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+        elixirls = {},
         --
 
         lua_ls = {
@@ -957,7 +958,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'elixir', 'typescript' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
