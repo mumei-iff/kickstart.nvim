28060365ff83750911c474792cf82d3a1e9eaa75
mumei-iff <mumei-iff@users.noreply.github.com>
2025-03-30 12:52:38 +0800

    update

--- hello.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-hello() {
-	echo "hello ${1}!" 
-}
-
-hello "TANTAN"
---
+++ lua/config/setup.lua
@@ -0,0 +1,50 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' },
+  },
+  install = { colorscheme = { 'kanagawa' } },
+  checker = { enabled = true },
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+require 'config.options'
+require 'config.keymaps'
+require 'kickstart.health'
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
